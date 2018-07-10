eb14e70960940795c14305d0ee4ef3197a6d9e01
Ananya Phadke <phadkeananya@gmail.com>
2018-07-10 17:58:29 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,9 @@
 # write your method here
 
-def crazy_strings(a,b)
-
+def crazy_strings(string, string2)
+  puts (string .reverse.upcase, string2 .swapcase.gsub (s), (z).gsub (S), (Z)
+  
 end
+
+crazy_strings ("string", "string2")
+
